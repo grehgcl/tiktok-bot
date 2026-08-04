03d1f8b98b256f41ebeab2541d0e30ab77300356
grehgcl <grehgcl@hotmail.com>
2026-08-03 22:50:49 -0300

    Adiciona modo headless para servidor

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 import json
 import os
 
-from bot_playwright import TikTokBot, get_bot
+from bot_cloud import TikTokBot, get_bot
 st.set_page_config(
     page_title="TikTok Bot - Engagement Automatico",
     page_icon="🤖",
